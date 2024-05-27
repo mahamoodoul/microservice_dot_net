210c1e004dc82a0cfac4841583eafcc24045c107
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-05-27 11:30:44 +0200

    login and register ui

---
+++ Mango.Web/Views/Auth/Login.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mango.Web.Views.Auth;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ Mango.Web/Views/Auth/Register.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mango.Web.Views.Auth;
+
+public class Register : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
