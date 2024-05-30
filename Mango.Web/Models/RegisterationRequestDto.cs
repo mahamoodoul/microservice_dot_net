0b830525c22b63248d563beb58fa561a4f35453f
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-05-30 12:04:01 +0200

    validation with form

--- Mango.Web/Models/RegisterationRequestDto.cs
+++ Mango.Web/Models/RegisterationRequestDto.cs
@@ -1,11 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Mango.Web.Models
 {
     public class RegistrationRequestDto
     {
-        public string Email { get; set; }
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
+        [Required] public string Email { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public string PhoneNumber { get; set; }
+        [Required] public string Password { get; set; }
         public string? Role { get; set; }
     }
 }
