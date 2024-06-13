8d2af43240462e2b848b1d1774fa6d8193caa9f9
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-06-14 00:20:23 +0200

    dynamic checkout

--- Mango.Web/Models/CartHeaderDto.cs
+++ Mango.Web/Models/CartHeaderDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Mango.Web.Models
 {
     public class CartHeaderDto
@@ -7,9 +9,11 @@ namespace Mango.Web.Models
         public string? CouponCode { get; set; }
         public double Discount { get; set; }
         public double CartTotal { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
         public string? Phone { get; set; }
+        [Required]
         public string? Email { get; set; }
     }
 }
