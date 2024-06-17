5cbdc198d13233edd13a258988d4e827bd21e350
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-06-18 00:16:15 +0200

    stripe coupon add and validate with place order

--- Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -65,8 +65,17 @@ namespace Mango.Services.OrderAPI.Controllers
                     CancelUrl = stripeRequestDto.CancelUrl,
                     LineItems = new List<SessionLineItemOptions>(),                     
                     Mode = "payment",
+                                        
                 };
 
+                var DiscountsObj = new List<SessionDiscountOptions>()
+                {
+                    new SessionDiscountOptions
+                    {
+                        Coupon=stripeRequestDto.OrderHeader.CouponCode
+                    }
+                };
+    
                 foreach(var item in stripeRequestDto.OrderHeader.OrderDetails)
                 {
                     var sessionLineItem = new SessionLineItemOptions
@@ -84,6 +93,11 @@ namespace Mango.Services.OrderAPI.Controllers
                     };
 
                     options.LineItems.Add(sessionLineItem);
+                }
+                
+                if (stripeRequestDto.OrderHeader.Discount > 0)
+                {
+                    options.Discounts = DiscountsObj;
                 }
 
 
